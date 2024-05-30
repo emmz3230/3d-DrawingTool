cdf6419ddf63190a56b79d277c4c6fa5bc8fc9a7
emmz3230 <emmz3230@gmail.com>
2024-05-30 15:11:10 +0100

    added shadow and asset link

--- src/components/Van_gogh_room.jsx
+++ src/components/Van_gogh_room.jsx
@@ -7,33 +7,118 @@ Source: https://sketchfab.com/3d-models/camera-limits-demo-van-gogh-bedroom-in-a
 Title: Camera Limits Demo: Van Gogh - Bedroom in Arles
 */
 
-import React, { useRef } from 'react'
-import { useGLTF } from '@react-three/drei'
+import React, { useRef } from "react";
+import { useGLTF } from "@react-three/drei";
+import { modelPath } from "/van_gogh_room.glb";
 
-export function Model(props) {
-  const { nodes, materials } = useGLTF('/van_gogh_room.glb')
+export function VanGoghRoom(exploreRoom, ...props) {
+  const { nodes, materials } = useGLTF(modelPath);
   return (
     <group {...props} dispose={null}>
-      <group rotation={[-Math.PI / 2, 0, 0]} scale={1.005}>
-        <mesh geometry={nodes.Object_2.geometry} material={materials['13___Default']} />
-        <mesh geometry={nodes.Object_3.geometry} material={materials['13___Defaultdddddd']} />
-        <mesh geometry={nodes.Object_4.geometry} material={materials['13___Defaultdddddddddddddd']} />
-        <mesh geometry={nodes.Object_5.geometry} material={materials['13___Defaultddddddddddddddddddd']} />
-        <mesh geometry={nodes.Object_6.geometry} material={materials['13___Defaultdddddddddddddddddddd']} />
-        <mesh geometry={nodes.Object_7.geometry} material={materials['13___Defaultd']} />
-        <mesh geometry={nodes.Object_8.geometry} material={materials['13___Defaultddd']} />
-        <mesh geometry={nodes.Object_9.geometry} material={materials['13___Defaultddddd']} />
-        <mesh geometry={nodes.Object_10.geometry} material={materials['13___Defaultdddddddd']} />
-        <mesh geometry={nodes.Object_11.geometry} material={materials['13___Defaultdddddddd']} />
-        <mesh geometry={nodes.Object_12.geometry} material={materials['13___Defaultddddddddd']} />
-        <mesh geometry={nodes.Object_13.geometry} material={materials['13___Defaultddddddddddd']} />
-        <mesh geometry={nodes.Object_14.geometry} material={materials['13___Defaultdddddddddddd']} />
-        <mesh geometry={nodes.Object_15.geometry} material={materials['13___Defaultddddddddddddd']} />
-        <mesh geometry={nodes.Object_16.geometry} material={materials['13___Defaultddddddddddddddd']} />
-        <mesh geometry={nodes.Object_17.geometry} material={materials['13___Defaultdddddddddddddddd']} />
+      <group
+        position={[-1.3, -1.9, 0]}
+        rotation={[-Math.PI / 2, 0, 0]}
+        scale={1.005}
+      >
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Object_2.geometry}
+          material={materials["13___Default"]}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Object_3.geometry}
+          material={materials["13___Defaultdddddd"]}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Object_4.geometry}
+          material={materials["13___Defaultdddddddddddddd"]}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Object_5.geometry}
+          material={materials["13___Defaultddddddddddddddddddd"]}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Object_6.geometry}
+          material={materials["13___Defaultdddddddddddddddddddd"]}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Object_7.geometry}
+          material={materials["13___Defaultd"]}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Object_8.geometry}
+          material={materials["13___Defaultddd"]}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Object_9.geometry}
+          material={materials["13___Defaultddddd"]}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Object_10.geometry}
+          material={materials["13___Defaultdddddddd"]}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Object_11.geometry}
+          material={materials["13___Defaultdddddddd"]}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Object_12.geometry}
+          material={materials["13___Defaultddddddddd"]}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Object_13.geometry}
+          material={materials["13___Defaultddddddddddd"]}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Object_14.geometry}
+          material={materials["13___Defaultdddddddddddd"]}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Object_15.geometry}
+          material={materials["13___Defaultddddddddddddd"]}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Object_16.geometry}
+          material={materials["13___Defaultddddddddddddddd"]}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Object_17.geometry}
+          material={materials["13___Defaultdddddddddddddddd"]}
+        />
       </group>
     </group>
-  )
+  );
 }
 
-useGLTF.preload('/van_gogh_room.glb')
+useGLTF.preload(modelPath);
