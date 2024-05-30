521d0c3a4847845eb8df95f5074fd017d9a29107
emmz3230 <emmz3230@gmail.com>
2024-05-30 14:51:44 +0100

    created  react component using npx gltf

---
+++ src/components/SketchApp/SketchApp.jsx
@@ -0,0 +1,14 @@
+import { PaintingBrush } from "../PaintingBrush/PaintingBrush";
+import { VanGoghAvatar } from "../VanGoghAvatar";
+
+export const SketchApp = ({
+  mousePosition,
+  setShowDrawingCanvas,
+}) => {
+  return (
+    <>
+      <PaintingBrush mousePosition={mousePosition} />
+      <VanGoghAvatar />
+    </>
+  );
+};
